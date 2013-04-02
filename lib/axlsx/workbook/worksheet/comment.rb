2da9ad62c6f4e6bb00ead30ee1295b8101314dcc
Jeremy Aube <jaube@roirevolution.com>
2013-04-02 08:55:29 -0400

    Fixes misspelling of visibility.

--- lib/axlsx/workbook/worksheet/comment.rb
+++ lib/axlsx/workbook/worksheet/comment.rb
@@ -12,7 +12,7 @@ module Axlsx
     # @option [String] author the author of the comment
     # @option [String] text The text for the comment
     # @option [String] ref The refence (e.g. 'A3' where this comment will be anchored.
-    # @option [Boolean] visible This controls the visiblity of the associated vml_shape.
+    # @option [Boolean] visible This controls the visibility of the associated vml_shape.
     def initialize(comments, options={})
       raise ArgumentError, "A comment needs a parent comments object" unless comments.is_a?(Comments)
       @visible = true
